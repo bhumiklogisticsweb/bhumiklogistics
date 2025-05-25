bc8ba0b48b0d94550b4064299bf8ffd5db3482d4
argustrack <argustrack@proton.me>
2025-05-25 09:08:08 +0530

    Initial commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: '*',//'https://yourfrontenddomain.com', // Replace with your real GoDaddy domain
+  origin: 'https://www.bhumiklogistics.com',//'https://yourfrontenddomain.com', // Replace with your real GoDaddy domain
   methods: ['POST'],
 }));
 mongoose.connect(process.env.MONGODB_URI)
